de5b16eb2688efbd135d330549bcc5d063880917
AdityaDongre24 <58044425+AdityaDongre24@users.noreply.github.com>
2020-10-05 14:51:21 +0530

    Add files via upload

---
+++ BlazorApp/Shared/LoginStatus.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Shared
+{
+    public class LoginStatus
+    {
+
+        private bool _IsLoggedIn;
+
+        public event Action OnLoggedIn;
+
+        public bool IsLoggedIn
+        {
+            get { return _IsLoggedIn; }
+            set {
+                _IsLoggedIn = value;
+                LoggInStausChanged();
+            }
+        }
+
+        private void LoggInStausChanged()
+        {
+            OnLoggedIn.Invoke();
+        }
+
+    }
+}
